325b9b093973aae78ba2ccb0370ac4a58dd69c70
kenshinwow1 <kenshinwow1@gmail.com>
2024-06-29 02:32:50 +0200

    uhilant

---
+++ src/Temperaturechek.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class  Temperaturechek{
+    public static void main(String[] args) {
+        String armi = "uhilant
+
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        if (age >= 18 && age <= 28) {
+
+                System.out.print(name + armi);
+            }
+scanner.close();
+    }
+}
+
